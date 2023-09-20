1c50587dd3fb6f7135c61dd39a55a44d8d1d4b9c
yasmenMohammed1 <yasmen.abodabash@gmail.com>
2023-09-20 19:03:37 +0300

    Feat:[navbar]-[lazy router]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy } from "react";
+import Navbar from "./utils/Componants/Navbar";
+import "./utils/Style/global.css";
+import "./utils/Style/main.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
 
+const Product = lazy(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(import("./Pages/Product")), 30000);
+  });
+});
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
       </header>
+      <div className="body-container">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route
+            path="product"
+            element={
+              <React.Suspense fallback={<>...</>}>
+                <Product />
+              </React.Suspense>
+            }
+          />
+          <Route path="*" element={<>404</>} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/utils/Componants/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export class Navbar extends Component {
+  navbarItems = [
+    "products",
+    "Best Seller",
+    "New Arrival",
+    "About Us",
+    "Contact Us",
+  ];
+  render() {
+    return (
+      <div className="navbar ">
+        <img src="/images/AlignLeftText.png" alt="menu icon" />
+        <img src="/images/logo.png" alt="logo" />
+        <div className="navbar-items  ">
+          {this.navbarItems.map((navbarItem) => (
+            <a key={navbarItem} href={navbarItem}>
+              {navbarItem}
+            </a>
+          ))}
+        </div>
+        <div className="col-md-3 icons-bar">
+          <img src="/images/search.png" alt="search icon" />{" "}
+          <img src="/images/cart.png" alt="cart icon" />{" "}
+          <img src="/images/profile.png" alt="profile avatar" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;

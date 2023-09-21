ea51ca8f366e8d4c631eba60b4b787d576676461
yasmenMohammed1 <yasmen.abodabash@gmail.com>
2023-09-21 21:44:29 +0300

    FEAT:fallback-page

---
+++ src/Pages/FallBack.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "../utils/Style/fallback.scss";
+export class FallBack extends Component {
+  render() {
+    const { percent } = this.props;
+    return (
+      <div className="fallback-container">
+        <img src="/images/div-logo.png" alt="logo" />
+        <div className="progress">
+          <div className="progress__bar" style={{ width: `${percent}%` }}></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FallBack;

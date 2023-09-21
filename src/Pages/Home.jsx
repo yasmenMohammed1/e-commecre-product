d84704fcdd2b2182aac523a2a02c2ffa98065948
yasmenMohammed1 <yasmen.abodabash@gmail.com>
2023-09-21 23:06:37 +0300

    fixes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return <div color="green">home </div>;
+    return <div>home </div>;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <React.StrictMode>
+    <BrowserRouter basename="/e-commecre-product/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

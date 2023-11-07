74d2c7b7eb46da62eb9669a6e5db4c0cd612bfeb
yasmenMohammed1 <yasmen.abodabash@gmail.com>
2023-11-07 12:46:22 +0200

    push

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/e-commecre-product/">
+    <HashRouter basename="/">
       <App />
     </HashRouter>
   </React.StrictMode>
